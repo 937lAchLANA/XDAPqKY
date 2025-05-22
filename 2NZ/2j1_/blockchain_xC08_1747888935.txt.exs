981c55f5ace241b47547aa9fa160595fb9e90e04
937lAchLANA <birding.seesaws_0u@icloud.com>
2025-05-22 16:47:28 +0200

    Improve the file's modularity by adding new classes

--- 2NZ/2j1_/blockchain_xC08_1747888935.txt.exs
+++ 2NZ/2j1_/blockchain_xC08_1747888935.txt.exs
@@ -99,7 +99,6 @@
           # You can also customize the exit_status of each check.
           # If you don't want TODO comments to cause `mix credo` to fail, just
           # set this value to 0 (zero).
-          #
           {Credo.Check.Design.TagTODO, [exit_status: 0]},
 
           #
